45d1d4a793ca10c5809bf08b9a4b622f18f570d3
James Brooks <kerspoon@gmail.com>
2016-02-05 12:45:08 +0000

    better google analytics tracking
    see http://jeroensormani.com/google-analytics-one-page-tracking/

--- js/agency.js
+++ js/agency.js
@@ -13,6 +13,19 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+
+    var runned = false;
+    $( window ).scroll( function() {
+      if ( $( document ).scrollTop() >= 500 && runned == false ) {
+        ga('send', 'event', { eventCategory: 'Scrolled 500px', eventAction: 'Scroll' });
+        runned = true;
+      }
+    });
+
+    setTimeout("ga('send', 'event', { eventCategory: '30 seconds on page', eventAction: 'Time' })", 30000);
+    setTimeout("ga('send', 'event', { eventCategory: '60 seconds on page', eventAction: 'Time' })", 60000);
+
 });
 
 // Highlight the top nav as scrolling occurs
@@ -24,3 +37,7 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+function clickEvent(name) {
+  ga('send', 'event', { eventCategory: name, eventAction: 'Click' });
+}
